cf786807f57c8c74a992f8f47fe6c0b6dd2f0101
Lucas da Silva <sklucassilva@gmail.com>
2025-05-19 21:30:48 -0300

    Refactor: Inclusão do favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
-import './globals.css'
+import './globals.css';
+import React, { ReactNode } from 'react';
 
-export const metadata = {
-      generator: 'v0.dev'
-    };
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="pt">
+      <head>
+        <title>SEM FOLEGO FC</title>
+        <meta name="description" content="EM FOLEGO FC" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
